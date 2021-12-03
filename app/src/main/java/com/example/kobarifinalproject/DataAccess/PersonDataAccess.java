16f2dff6ef85a20a069a34db7705ce763ae74a0c
Kobari Xiong <Kobaribeef@gmail.com>
2021-12-02 18:56:49 -0600

    Connected final project to Firebase

--- app/src/main/java/com/example/kobarifinalproject/DataAccess/PersonDataAccess.java
+++ app/src/main/java/com/example/kobarifinalproject/DataAccess/PersonDataAccess.java
@@ -1,4 +1,18 @@
 package com.example.kobarifinalproject.DataAccess;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Collection;
+
 public class PersonDataAccess {
+
+    public static final String TAG = "PersonDataAccess";
+    private static FirebaseFirestore db;
+    CollectionReference peopleCollection;
+
+    public PersonDataAccess(){
+        db = FirebaseFirestore.getInstance();
+        peopleCollection = db.collection("people");
+    }
 }
