d54d8e8bf744752926c14228700b3289835d5679
Kobari Xiong <Kobaribeef@gmail.com>
2021-12-04 09:28:45 -0600

    Worked on Person Data Access

--- app/src/main/java/com/example/kobarifinalproject/DataAccess/FirebaseListener.java
+++ app/src/main/java/com/example/kobarifinalproject/DataAccess/FirebaseListener.java
@@ -1,4 +1,5 @@
 package com.example.kobarifinalproject.DataAccess;
 
 public interface FirebaseListener {
+    public void done(Object obj);
 }
--- app/src/main/java/com/example/kobarifinalproject/DataAccess/PersonDataAccess.java
+++ app/src/main/java/com/example/kobarifinalproject/DataAccess/PersonDataAccess.java
@@ -1,9 +1,22 @@
 package com.example.kobarifinalproject.DataAccess;
 
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.FirebaseFirestore;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
+
+import com.example.kobarifinalproject.models.Person;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 
 public class PersonDataAccess {
 
@@ -14,5 +27,44 @@ public class PersonDataAccess {
     public PersonDataAccess(){
         db = FirebaseFirestore.getInstance();
         peopleCollection = db.collection("people");
+    }
+
+    public void getAllPeople(FirebaseListener listener){
+        peopleCollection.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                List<DocumentSnapshot> docs = queryDocumentSnapshots.getDocuments();
+                List<Person> personObjects = new ArrayList();
+
+                for(DocumentSnapshot p : docs){
+                    Person person = convertDocumentToPerson(p);
+                    personObjects.add(person);
+                }
+
+                listener.done(personObjects);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "Unable to get all people" + e.toString());
+
+            }
+        });
+    }
+
+    private Person convertDocumentToPerson(DocumentSnapshot doc){
+        String personId = doc.getId();
+        try{
+            String personName = doc.getString("name");
+            String personDesc = doc.getString("description");
+            Timestamp ts = doc.getTimestamp("firstMet");
+            Date firstMet = ts.toDate();
+            boolean met = doc.getBoolean("met");
+            Person person = new Person(personId, personName, personDesc, met, firstMet);
+            return person;
+        }catch(Exception e){
+            Log.d(TAG, "UNABLE TO CONVERT DOCUMENT TO DOG" + e.toString());
+            return null;
+        }
     }
 }
